4f052cc06038d6c9de1beb7e4a3ce3070105ab3d
Joseph Haaga <haaga.joe@gmail.com>
2020-04-17 18:29:43 -0400

    adding and listing!

--- todos/main.py
+++ todos/main.py
@@ -1,20 +1,31 @@
 import click
+from tinydb import TinyDB, Query
+from datetime import datetime as dt
+
+db = TinyDB('db.json')
+
+@click.group()
+def cli():
+    pass
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
-
-def list_todos():
+def list():
     """List all todos in the database."""
-    for num in range(5):
-        click.echo(f'todo #{num}')
+    todos = db.all()
+    for todo in todos: 
+        click.echo(todo)
     
-    
+@click.command()
+@click.argument('description')
+def add(description):
+    todo = {'description': description, 'inserted_at': dt.utcnow().isoformat()}
+    db.insert(todo)
+    click.echo(f'Inserted {todo}')
+
+
+cli.add_command(add)
+cli.add_command(list)
+
 
 if __name__ == '__main__':
-    list_todos()
+    cli() 
