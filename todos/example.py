081a5e9698ce9b360cd97b902876eeb7a5254377
Joseph Haaga <haaga.joe@gmail.com>
2020-04-23 10:23:57 -0400

    ideas

---
+++ todos/example.py
@@ -0,0 +1,7 @@
+import requests
+# testing black from vi
+class FakeClass
+   s = 'a'
+   m = 'b'
+    def func(a):
+        return a + 1 
--- todos/main.py
+++ todos/main.py
@@ -12,37 +12,46 @@ Status = Enum("Status", "NOT_STARTED IN_PROGRESS COMPLETED")
 def cli():
     pass
 
+
 @click.command()
-@click.option('--all', 'status', flag_value='ALL', default=True)
-@click.option('--not-started', 'status', flag_value='NOT_STARTED')
-@click.option('--in-progress', 'status', flag_value='IN_PROGRESS')
-@click.option('--completed', 'status', flag_value='COMPLETED')
-@click.option('-t', '--tag', 'tag',  default=False)
+@click.option("--all", "status", flag_value="ALL", default=True)
+@click.option("--not-started", "status", flag_value="NOT_STARTED")
+@click.option("--in-progress", "status", flag_value="IN_PROGRESS")
+@click.option("--completed", "status", flag_value="COMPLETED")
+@click.option("-t", "--tag", "tag", default=False)
 def list(status, tag=False):
     """List all todos in the database."""
     click.echo(f"Searching for todos with status: {status} and tag: {tag}")
     contains_tag = lambda tags: (tag in tags) if tag else True
     # contains_tag = lambda tags: len(tags) > 0
-    status_matches = lambda todo_status: True if status == 'ALL' else todo_status == Status[status].value
+    status_matches = (
+        lambda todo_status: True
+        if status == "ALL"
+        else todo_status == Status[status].value
+    )
 
     Todo = Query()
-    todos = db.search((Todo.status.test(status_matches)) & (Todo.tags.test(contains_tag)))
+    todos = db.search(
+        (Todo.status.test(status_matches)) & (Todo.tags.test(contains_tag))
+    )
     for todo in todos:
         click.echo(f"#{todo.doc_id}: {todo}")
 
+
 @click.command()
 @click.argument("description")
-@click.option('-t', '--tags')
+@click.option("-t", "--tags")
 def add(description, tags=[]):
     """Add a todo item to the database."""
     todo = {
         "description": description,
         "inserted_at": dt.utcnow().isoformat(),
         "status": Status.NOT_STARTED.value,
-        "tags": tags.split(",") if tags else []
+        "tags": tags.split(",") if tags else [],
     }
     doc_id = db.insert(todo)
     click.echo(f"Inserted TODO #{doc_id}: {todo}")
+
 
 @click.command()
 @click.argument("todo_id", type=int)
@@ -57,6 +66,7 @@ def start(todo_id):
         doc_ids=[todo_id],
     )
     click.echo(f"Started TODO #{todo_id}: {db.get(doc_id=todo_id)}")
+
 
 @click.command()
 @click.argument("todo_id", type=int)
