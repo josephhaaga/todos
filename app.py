174ee5154fa0985ba805e24fd4dad5675b2fa4b4
Joseph Haaga <haaga.joe@gmail.com>
2020-05-04 21:04:39 -0400

    really bad app factory pattern

--- app.py
+++ app.py
@@ -18,8 +18,12 @@ def create_app():
     Path(app_instance_directory).mkdir(parents=True, exist_ok=True)
 
     path_to_db_file = path.join(app_instance_directory, 'db.json') 
-    todo_service = TodoService(Database(path_to_db_file))
 
+    app = type("App", (), {})
+    app.todo_service = TodoService(Database(path_to_db_file))
+    return app
+
+def create_cli(app):
     @click.group()
     def cli():
         pass
@@ -30,7 +34,7 @@ def create_app():
     @click.option("--in-progress", "status", flag_value="IN_PROGRESS")
     @click.option("--completed", "status", flag_value="COMPLETED")
     def show(status, tag=None):
-        todos = todo_service.list(status, tag)
+        todos = app.todo_service.list(status, tag)
         for todo in todos:
             click.echo(todo)
 
@@ -39,19 +43,18 @@ def create_app():
     @click.option("-t", "--tags")
     def add(title, tags=[]):
         """Add a todo item to the database."""
-        doc_id = todo_service.create(title, tags) 
+        doc_id = app.todo_service.create(title, tags) 
         click.echo(f"Inserted TODO #{doc_id}: {title}")
     
     @click.command()
     def then():
         """Suggest tasks to a user, starting a todo if they respond Y."""
-        # TODO warn user that they have IN_PROGRESS tasks. "Finish what you've already started!"
-        not_started_todos = todo_service.list("NOT_STARTED")
+        not_started_todos = app.todo_service.list("NOT_STARTED")
         for task in not_started_todos:
             response = True if input(f"Start {task}? (Y/N) ") == 'Y' else False
             if response:
                 # TODO tech debt
-                start(str(task.id))
+                start(task.id)
                 return True
         pass
 
@@ -59,27 +62,25 @@ def create_app():
     @click.argument("todo_id", type=int)
     def start(todo_id):
         """Start working on a todo item."""
-        started_todo = todo_service.start(todo_id)
+        started_todo = app.todo_service.start(todo_id)
         click.echo(f"Started {started_todo}")
 
     @click.command()
     @click.argument("todo_id", type=int)
     def complete(todo_id):
         """Finish working on a todo item."""
-        finished_todo = todo_service.complete(todo_id)
+        finished_todo = app.todo_service.complete(todo_id)
         click.echo(f"Finished {finished_todo}")
 
     @click.command()
     @click.argument("todo_id", type=int)
     def delete(todo_id):
         """Delete a todo item."""
-        result = todo_service.delete(todo_id)
+        result = app.todo_service.delete(todo_id)
         if result:
             click.echo(f"Successfully deleted.")
         else:
             click.echo(f"Error occurred while deleting {todo_id}")
-
-        click.echo()
 
     cli.add_command(show)
     cli.add_command(add)
@@ -91,4 +92,5 @@ def create_app():
     return cli()
 
 if __name__ == "__main__":
-    create_app()
+    app = create_app()
+    create_cli(app)
