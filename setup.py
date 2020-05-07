10d6cf66411ca215e98670923f7d8b49405e4d3b
Joseph Haaga <haaga.joe@gmail.com>
2020-05-06 22:04:13 -0400

    ran the build command

--- setup.py
+++ setup.py
@@ -6,17 +6,22 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-        name="jhaaga-todos",
-        version="1.0.0",
-        description="Manage tasks from the command line",
-        long_description=README,
-        long_description_content_type="text/markdown",
-        url="https://github.com/josephhaaga/todos",
-        author="Joseph Haaga",
-        author_email="haaga.joe@gmail.com",
-        license="MIT",
-        packages=["todos"],
-        include_package_data=True,
-        install_requires=["tinydb", "click"],
-    )
+    name="jhaaga-todos",
+    version="1.0.0",
+    description="Manage tasks from the command line",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/josephhaaga/todos",
+    author="Joseph Haaga",
+    author_email="haaga.joe@gmail.com",
+    license="MIT",
+    packages=["todos"],
+    include_package_data=True,
+    install_requires=["tinydb", "click"],
+    entry_points={
+        "console_scripts": [
+            "todo=todos.__main__:main",
+        ]
+    },
+)
 
