9c418cab825c67716b0ffd4447245c0e72e49151
Joseph Haaga <haaga.joe@gmail.com>
2020-05-06 21:54:30 -0400

    add setup.py

---
+++ todos/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+# Version of the jhaaga-todos package
+__version__ = "1.0.0"
---
+++ todos/__main__.py
@@ -0,0 +1,30 @@
+# Application Factory
+# Set up DB
+# Set up Service with connection to DB
+# Construct CLI and add functions
+# Return application instance
+
+from todos.service import TodoService
+from todos.db import Database
+from todos.utils import create_cli
+
+from pathlib import Path
+from os import path
+
+
+def create_app():
+    # Create or open instance folder, and pass to Database
+    home = str(Path.home())
+    app_instance_directory = path.join(home, ".todos")
+    Path(app_instance_directory).mkdir(parents=True, exist_ok=True)
+
+    path_to_db_file = path.join(app_instance_directory, "db.json")
+
+    app = type("App", (), {})
+    app.todo_service = TodoService(Database(path_to_db_file))
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    create_cli(app)
