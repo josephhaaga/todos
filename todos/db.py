70d73be07eb651bf21afc0f1ef46ff932db1b18e
Joseph Haaga <haaga.joe@gmail.com>
2020-05-11 21:11:23 -0400

    refactor Database

--- todos/db.py
+++ todos/db.py
@@ -1,5 +1,9 @@
-from tinydb import TinyDB, Query, where
+"""Interface and implementation providing database functionality."""
 from functools import reduce
+from typing import int, List
+
+from tinydb import TinyDB, where
+from todos.models import Todo
 
 
 class Database:
@@ -8,16 +12,16 @@ class Database:
     def __init__(self, path_to_database_file="db.json"):
         self.db = TinyDB(path_to_database_file)
 
-    def create(self, item: dict):
+    def create(self, item: dict) -> int:
         """Insert an item into the database."""
         document_id = self.db.insert(item)
         return document_id
 
-    def list(self):
+    def list(self) -> List:
         """Return all items in the database."""
         return self.db.all()
 
-    def find(self, clauses: dict):
+    def find(self, clauses: dict) -> List:
         """Search the database for items matching a set of criteria."""
         if len(clauses) == 0:
             return self.list()
@@ -26,18 +30,14 @@ class Database:
         built_query = reduce(lambda a, b: a & b, terms)
         return self.db.search(built_query)
 
-    def get(self, document_id: int):
+    def get(self, document_id: int) -> Todo:
         """Retrieve an item from the database."""
-        # document_id = int(document_id)
         return self.db.get(doc_id=document_id)
 
-    def update(self, document_id: int, updates_to_make: dict):
+    def update(self, document_id: int, updates_to_make: dict) -> None:
         """Update an item in the database."""
-        # document_id = int(document_id)
-        updated_doc_ids = self.db.update(updates_to_make, doc_ids=[document_id])
-        return updated_doc_ids[0]
+        self.db.update(updates_to_make, doc_ids=[document_id])
 
-    def delete(self, document_id: int):
+    def delete(self, document_id: int) -> None:
         """Remove an item from the database."""
-        # document_id = int(document_id)
-        return self.db.remove(doc_ids=[document_id])
+        self.db.remove(doc_ids=[document_id])
