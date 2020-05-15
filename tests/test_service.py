12c6f27e2f81b1a1aca1e894423f4e5be5760c1c
Joseph Haaga <haaga.joe@gmail.com>
2020-05-15 09:36:04 -0400

    fix tests in light of service refactor

--- tests/test_service.py
+++ tests/test_service.py
@@ -9,6 +9,7 @@ from todos.db import Database
 class TestTodoService:
     @pytest.fixture(scope="module")
     def db_backed_service(self):
+        # TODO This should be only in db.py. Service.py tests should rely on a stubbed DB
         test_db_file = "./test_db.json"
         db = Database(test_db_file)
         serv = TodoService(db)
@@ -32,18 +33,21 @@ class TestTodoService:
         assert (len(tasks)) == 1
 
     def test_estimate_time(self, db_backed_service):
-        task = db_backed_service.estimate_time(1, 4.0)
+        db_backed_service.estimate_time(1, 4.0)
+        # How can we alter this test so we don't rely on .get()?
+        task = db_backed_service.get(1)
         assert task.estimate_in_hours == 4.0
 
     def test_start(self, db_backed_service):
-        task = db_backed_service.start(1)
+        db_backed_service.start(1)
+        task = db_backed_service.get(1)
         assert "started_at" in task.__dict__
 
     def test_complete(self, db_backed_service):
-        task = db_backed_service.complete(1)
+        db_backed_service.complete(1)
+        task = db_backed_service.get(1)
         assert "completed_at" in task.__dict__
 
     def test_delete(self, db_backed_service):
-        task_id = db_backed_service.delete(1)
-        assert task_id == 1
+        db_backed_service.delete(1)
         assert len(db_backed_service.list()) == 0  # smelly
