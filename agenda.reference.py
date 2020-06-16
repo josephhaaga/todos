fff63dd95faa13b078e49bc42d1ec4e4e7a57757
Joseph Haaga <haaga.joe@gmail.com>
2020-06-15 20:09:05 -0400

    clean up

--- agenda.reference.py
+++
@@ -1,5 +0,0 @@
-def fill_todos(todos):
-    for todo in todos:
-        estimated_duration = todo.estimate_in_hours
-        if self.has_free_time_block(estimated_duration):
-            self.add_event(CalendarEvent(**todo))
